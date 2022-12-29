180b453a5edf04a1fb01348cdde1b2a88130039d
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-28 17:23:43 -0800

    setup the main page UI using cardview and disable the statusbar

--- app/src/main/java/com/ted/quiz/MainActivity.kt
+++ app/src/main/java/com/ted/quiz/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ted.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // To hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
